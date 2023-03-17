ae27412205968035505e7a93b223be202be946a8
Chase Bennett-Hill (1000106289) <BENNC9@student.op.ac.nz>
2023-03-17 13:41:40 +1300

    Enemy Health and shooting added

--- Assets/Scripts/MoveEnemy.cs
+++ Assets/Scripts/MoveEnemy.cs
@@ -44,6 +44,17 @@ public class MoveEnemy : MonoBehaviour
             }
         }
     }
+    public float DistanceToGoal()
+    {
+        float distance = Vector2.Distance(gameObject.transform.position, waypoints[currentWaypoint + 1].transform.position);
+        for (int i = currentWaypoint + 1; i < waypoints.Length - 1; i++)
+        {
+            Vector3 startPosition = waypoints[i].transform.position;
+            Vector3 endPosition = waypoints[i + 1].transform.position;
+            distance += Vector2.Distance(startPosition, endPosition);
+        }
+        return distance;
+    }
 
 
     private void RotateIntoMoveDirection()
